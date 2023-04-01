ce57c9efba44bb105a2d8de5b1f05d593f796749
radz <Aditya@gmail.com>
2023-04-01 23:26:44 +0530

    modified smita.txt

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Router from "./Components/Router";
 import { Provider } from "react-redux";
 import configureStore from "./Container/Store/configureStore";
 
-let store = configureStore();
+let store = configureStore(); /djfbdj
 
 ReactDOM.render(
   <Provider store={store}>
